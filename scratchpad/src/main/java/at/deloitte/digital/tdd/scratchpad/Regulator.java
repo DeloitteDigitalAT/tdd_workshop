90a9a52fe05612f0023520c48b78dd7e39091958
Jean D'Amore <jdamore@deloitte.at>
2019-04-04 13:49:35 +0200

    Cleaned-up scratchpad

--- scratchpad/src/main/java/at/deloitte/digital/tdd/scratchpad/Regulator.java
+++ scratchpad/src/main/java/at/deloitte/digital/tdd/scratchpad/Regulator.java
@@ -3,17 +3,8 @@ package at.deloitte.digital.tdd.scratchpad;
 
 public class Regulator {
 
-    private Thermometer thermometer;
+    public int regulate(int desiredTemperature, int currentTemperature) {
 
-    public Regulator(Thermometer thermometer) {
-        this.thermometer = thermometer;
-    }
-
-    public int regulate(int desiredTemperature) {
-
-        if(desiredTemperature<0) {
-            throw new IllegalArgumentException("The desired temperature cannot be >0");
-        }
-        return Integer.signum(desiredTemperature - thermometer.readTemperature());
+        return -1;
     }
 }
--- scratchpad/src/test/java/at/deloitte/digital/tdd/scratchpad/RegulatorTest.java
+++ scratchpad/src/test/java/at/deloitte/digital/tdd/scratchpad/RegulatorTest.java
@@ -1,71 +1,23 @@
 package at.deloitte.digital.tdd.scratchpad;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.Random;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
 public class RegulatorTest {
-
-    private Regulator regulator;
-
-    @Mock private Thermometer mockThermometer;
-    private int currentTemperature;
 
     @Before
     public void setup() {
-        currentTemperature = 20;
-        when(mockThermometer.readTemperature()).thenReturn(currentTemperature);
-        regulator = new Regulator(mockThermometer);
+    }
+
+    @After
+    public void tearDown() {
     }
 
     @Test
-    public void shouldReturnPlusOne() {
-
-        int temperatureIncement =
-                regulator.regulate(currentTemperature+5);
-
-        assertEquals(1, temperatureIncement);
-    }
-
-    @Test
-    public void shouldReturnMinusOne() {
-
-        int temperatureIncement =
-                regulator.regulate(currentTemperature-5);
-
-        assertEquals(-1, temperatureIncement);
-    }
-
-    @Test
-    public void shouldReturnZero() {
-
-        int temperatureIncement =
-                regulator.regulate(currentTemperature);
-
-        assertEquals(0, temperatureIncement);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowIllegalArgumentExceptionWhenDesiredTemperatureIsNegative() {
-        regulator.regulate(-1);
-    }
-
-    @Test
-    public void shouldNotThrowIllegalArgumentExceptionWhenCurrentTemperatureIsNegative() {
-        try {
-            when(mockThermometer.readTemperature()).thenReturn(-1);
-            regulator.regulate(new Random().nextInt(20));
-        } catch (IllegalArgumentException e) {
-            throw new AssertionError(
-                    "Should not throw IllegalArgumentException if current temperature is >0");
-        }
+    public void shouldRegulate() {
+        assertTrue(true);
     }
 }
